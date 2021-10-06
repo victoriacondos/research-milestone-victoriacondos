3ef7f276877d246bf15f16d250989da5fbb9ec91
Victoria Condos <vcondos@email.sc.edu>
2021-10-06 19:14:01 -0400

    added basic outline of my application

--- App.js
+++ App.js
@@ -1,22 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { setStatusBarNetworkActivityIndicatorVisible, StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const[name, setName] = useState('Victoria'); {/*Use this for when you are taking inputted titles and adding them to summary for actual project */}
+  const[entryTitle, setEntryTitle] = useState('none'); {/*for adding together, do entry.amount addition*/}
+  const[entryAmount, setEntryAmount] = useState(0);
+  const[netTotal, setNetTotal] = useState(0)
+
+  const clickAddEntry = () => {
+    {/* add function here to save last entry to list and add value to running total 
+    (using entryTitle and entryAmount variables (append them to a string?) add to total variable*/}
+  }
+
+  const clickCalculate = () => {
+    
+    {/*add function here to display all entries and calcualte net total of entries*/}
+    
+  }
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Victoria's Research Milestone Sample Project!</Text>
-      
-      <StatusBar style="auto" />
-    </View>
+    <View  style={styles.container}>
+      <View style={styles.header} >
+        <Text style={styles.boldText}>Welcome to {name}'s Research Milestone Sample Project!</Text>
+        <Text  style={styles.body}>Create expense entries by inputting a title and a positive or negative value below then hitting 'Add Entry', and hit 'Calculate Net Total' to calculate the net total of your income and expenses!</Text>
+        <Text>Entry Title:</Text>
+        <TextInput 
+          style={styles.inputContainer} 
+          placeholder= 'i.e. Groceries'
+          onChangeText={(val) => setEntryTitle(val)}
+        />
+        <Text>Entry Value:</Text>
+        <TextInput 
+          style={styles.inputContainer} 
+          placeholder = 'i.e. -24.75'
+          onChangeText={(val) => setEntryAmount(val)}
+        />
+        
+        <Text></Text>
+        <View style={styles.buttonContainer}>
+          <Button title='Add Entry' onPress={clickAddEntry}/>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title='Calculate Net Total' onPress={clickCalculate}/>
+        </View>
+      </View>
+  </View> 
   );
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
+  container: { 
     flex: 1,
-    backgroundColor: '#b3ccc',
+    backgroundColor: 'rgb(99, 143, 148)',
     alignItems: 'center',
     justifyContent: 'center',
+  
   },
+
+  header: {
+    
+    padding: 20,
+    textAlignVertical: 'top',
+    
+  },
+
+  boldText:{
+    textAlign: 'center', 
+    textAlignVertical: 'top',
+    fontWeight: 'bold',
+  },
+  
+  body:{
+    padding: 20,
+    textAlign: 'center', 
+  },
+
+  buttonContainer:{
+    marginTop: 20,
+    marginHorizontal: 40,
+    borderWidth: 2,
+    borderColor: 'rgb(48, 103, 110)',
+    backgroundColor: '#fff',
+
+  },
+
+  inputContainer:{
+    marginVertical: 5,
+    marginHorizontal: 5,
+    padding: 8,
+    borderWidth: 2,
+    borderColor: 'rgb(48, 103, 110)',
+    backgroundColor: '#fff',
+  },
+  
+  
 });
